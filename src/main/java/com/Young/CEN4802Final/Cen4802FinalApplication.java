7e155407b63a9a56387981791d1e1c97dabb25fe
Tristin-Young <86166175+Tristin-Young@users.noreply.github.com>
2023-12-04 15:56:35 -0500

    Added Spring project

---
+++ src/main/java/com/Young/CEN4802Final/Cen4802FinalApplication.java
@@ -0,0 +1,13 @@
+package com.Young.CEN4802Final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cen4802FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cen4802FinalApplication.class, args);
+	}
+
+}
